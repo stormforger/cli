52d66b60b48f360f8711494ff236c7e380f1c753
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2017-02-26 18:49:36 +0100

    add datasource -> validate command

---
+++ cmd/datasource.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var DatasourceCmd = &cobra.Command{
+	Use:   "datasource",
+	Short: "Work with and manage data sources",
+	Long: `Work with and manage data sources.
+
+	Currently only a rough validation is implemented.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Fatal("Cannot be run without subcommand, like validate!")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(DatasourceCmd)
+}
---
+++ datasource/csv_test.go
@@ -0,0 +1,71 @@
+package datasource
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+var (
+	invalidCsv string
+	validCsv   string
+)
+
+func TestMain(m *testing.M) {
+	invalidCsvBytes, err := ioutil.ReadFile("../testdata/csv/invalid.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	invalidCsv = string(invalidCsvBytes)
+
+	validCsvBytes, err := ioutil.ReadFile("../testdata/csv/valid.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	validCsv = string(validCsvBytes)
+
+	os.Exit(m.Run())
+}
+
+func TestValidateCSVPositive(t *testing.T) {
+	r := strings.NewReader(validCsv)
+
+	results, err := ValidateCSV(r, DefaultValidatorOptions())
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(results) > 0 {
+		t.Error("Expected no errors, got ", results)
+	}
+}
+
+func TestValidateCSVNegative(t *testing.T) {
+	r := strings.NewReader(invalidCsv)
+
+	results, err := ValidateCSV(r, DefaultValidatorOptions())
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := []CsvError{
+		{Row: 2, Column: 2, Message: "Fields may not contain newlines!"},
+		{Row: 3, Column: 2, Message: "Fields may not contain the quoting character (\")!"},
+		{Row: 17, Column: 1, Message: "Fields may not contain the column separator (;)!"},
+	}
+
+	if len(results) != len(expected) {
+		t.Error("Expected ", len(expected), " errors, got ", len(results), results)
+		return
+	}
+
+	for index := range results {
+		if actual, expected := results[index], expected[index]; actual != expected {
+			t.Error("Expected ", expected, " got ", actual)
+		}
+	}
+}
