f26ccf0e34ce0b0ef2531cb8e9e7d5eeec16c02e
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2017-02-28 18:21:07 +0100

    remove comment

--- cmd/root.go
+++ cmd/root.go
@@ -33,11 +33,6 @@ const (
 	ENV_PREFIX      = "stormforger"
 )
 
-// Execute
-// searchs for JWT-token,
-// adds all child commands to the root command
-// and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	setupConfig()
 
