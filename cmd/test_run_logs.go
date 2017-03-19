41555d31c06a505fa1cc94b6feb5f6fe6d76ff8e
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2017-03-19 16:44:29 +0100

    add --type option to logs command

--- cmd/test_run_logs.go
+++ cmd/test_run_logs.go
@@ -7,11 +7,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// calllogCmd represents the calllog command
-var calllogCmd = &cobra.Command{
-	Use:   "clog <test-run-ref>",
-	Short: "Fetch up to 10k lines of the test runs call log",
-	Long: `Will fetch up to 10k lines of the test runs call log.
+var (
+	// calllogCmd represents the calllog command
+	calllogCmd = &cobra.Command{
+		Use:   "logs <test-run-ref>",
+		Short: "Fetch up to 10k lines of the test runs call log",
+		Long: `Will fetch up to 10k lines of the test runs call log.
 
 The call log contains:
   * time (epoch in seconds)
@@ -22,16 +23,27 @@ The call log contains:
   * response size (in Bytes)
   * duration (in ms)
   * request tag`,
-	Run: run,
-}
+		Run: run,
+	}
+
+	logOpts struct {
+		Type string
+	}
+)
 
 func init() {
 	TestRunCmd.AddCommand(calllogCmd)
+
+	calllogCmd.Flags().StringVar(&logOpts.Type, "type", "request", "type of logs")
 }
 
 func run(cmd *cobra.Command, args []string) {
 	if len(args) != 1 {
 		log.Fatal("Expect exactly one argument: Test Run Reference")
+	}
+
+	if logOpts.Type != "request" {
+		log.Fatal(fmt.Sprintf("Unsupported log type %s", logOpts.Type))
 	}
 
 	client := NewClient()
