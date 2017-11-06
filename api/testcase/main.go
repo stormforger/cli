45af2126970518a2397b0a9854f1ad1620132e6d
Sebastian Cohnen <tisba@users.noreply.github.com>
2017-11-06 17:52:43 +0100

    add test-case list command

--- api/testcase/main.go
+++ api/testcase/main.go
@@ -1,5 +1,14 @@
 package testcase
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"reflect"
+
+	"github.com/google/jsonapi"
+)
+
 // List is a list of TestCases, used for index action
 type List struct {
 	TestCases []*TestCase
@@ -10,4 +19,19 @@ type TestCase struct {
 	ID          string `jsonapi:"primary,test_cases"`
 	Name        string `jsonapi:"attr,name"`
 	Description string `jsonapi:"attr,description"`
+}
+
+// ShowNames displays the name and uid of organisations
+func ShowNames(input io.Reader) {
+	items, err := jsonapi.UnmarshalManyPayload(input, reflect.TypeOf(new(TestCase)))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, item := range items {
+		testCase, _ := item.(*TestCase)
+
+		fmt.Printf("* %s (ID: %s)\n", testCase.Name, testCase.ID)
+	}
 }
