b2d0bcce8a869995507c681ee54d43dc291cd1dd
Sebastian Cohnen <tisba@users.noreply.github.com>
2017-11-28 19:54:37 +0100

    Add --watch to test-case launch command (#17)
    * extract watchTestRun to utils
    * add --watch option to test-case launch command

--- cmd/utils.go
+++ cmd/utils.go
@@ -3,12 +3,14 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 func readFromStdinOrReadFirstArgument(args []string, defaultFileName string) (fileName string, reader io.Reader, err error) {
@@ -52,4 +54,35 @@ func readOrganisationUIDFromFile() string {
 	}
 
 	return strings.TrimSpace(string(content))
+}
+
+func watchTestRun(testRunUID string, maxWatchTime int) {
+	client := NewClient()
+
+	started := time.Now()
+
+	for true {
+		runningSince := time.Now().Sub(started).Seconds()
+
+		testRun, response, err := client.TestRunWatch(testRunUID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(response)
+
+		if !testRunOkay(&testRun) {
+			os.Exit(1)
+		}
+
+		if testRunSuccess(&testRun) {
+			os.Exit(0)
+		}
+
+		if maxWatchTime > 0 && int(runningSince) > maxWatchTime {
+			os.Exit(2)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
