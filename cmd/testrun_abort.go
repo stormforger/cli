f8c3746b1be766a30510cef92b844c50c997b0f5
Sebastian Cohnen <tisba@users.noreply.github.com>
2018-05-04 21:57:05 +0200

    check argument count for test-run {abort,show,watch} (#39)

--- cmd/testrun_abort.go
+++ cmd/testrun_abort.go
@@ -15,6 +15,11 @@ var (
 		Short: "Abort the given running test",
 		Long:  `Abort the given running test.`,
 		Run:   testRunAbort,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				log.Fatal("Expect exactly one argument: test run reference!")
+			}
+		},
 	}
 )
 
