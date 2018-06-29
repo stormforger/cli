24571d56f61111b3e635839c4439cfeeb8ff0c13
Sebastian Cohnen <tisba@users.noreply.github.com>
2018-06-29 16:54:41 +0200

    Add --nfr-check-file Option to Test Case Launch Command (#46)

--- cmd/testrun_nfr.go
+++ cmd/testrun_nfr.go
@@ -1,14 +1,9 @@
 package cmd
 
 import (
-	"bytes"
-	"fmt"
 	"log"
-	"os"
 
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
-	"github.com/stormforger/cli/api/testrun"
 )
 
 var (
@@ -43,71 +38,5 @@ func testRunNfrRun(cmd *cobra.Command, args []string) {
 		log.Fatal(err)
 	}
 
-	status, result, err := client.TestRunNfrCheck(testRunUID, fileName, file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if rootOpts.OutputFormat == "json" {
-		fmt.Println(string(result))
-		return
-	}
-
-	if !status {
-		log.Fatalf("Could not perform test run NFR checks...\n%s", result)
-	}
-
-	items, err := testrun.UnmarshalNfrResults(bytes.NewReader(result))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	green := color.New(color.FgGreen).SprintFunc()
-	red := color.New(color.FgRed).SprintFunc()
-	redBg := color.New(color.BgRed).Add(color.FgWhite).SprintFunc()
-	white := color.New(color.FgWhite).SprintFunc()
-
-	checkStatus := ""
-	anyFails := false
-	for _, item := range items.NfrResults {
-		if !item.Disabled {
-			actualSubject := ""
-			if item.Success {
-				checkStatus = green("\u2713")
-				actualSubject = fmt.Sprintf("was %s", item.SubjectWithUnit())
-			} else {
-				anyFails = true
-				checkStatus = red("\u2717")
-				actualSubject = fmt.Sprintf("but actually was %s", item.SubjectWithUnit())
-			}
-
-			filter := ""
-			if item.Filter != "null" && item.Filter != "" {
-				filter = " (where: " + item.Filter + ")"
-			}
-
-			fmt.Printf(
-				"%s %s expected to be %s; %s (%s)%s\n",
-				checkStatus,
-				item.Metric,
-				item.ExpectationWithUnit(),
-				actualSubject,
-				item.Type,
-				filter,
-			)
-		} else {
-			fmt.Printf(
-				"%s %s %s expected to be %s (%s)\n",
-				white("?"),
-				redBg("DISABLED"),
-				item.Metric,
-				item.ExpectationWithUnit(),
-				item.Type,
-			)
-		}
-	}
-
-	if anyFails {
-		os.Exit(1)
-	}
+	runNfrCheck(*client, testRunUID, fileName, file)
 }
