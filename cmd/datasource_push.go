b80cd34e2b12bab707951a4fd4f2ad9e5d0d7bf7
Sebastian Cohnen <tisba@users.noreply.github.com>
2018-07-03 10:04:49 +0200

    Data Sources: Add --auto-field-names (#47)
    * add --auto-field-names to ds push command
    
    * add --auto-field-names to README
    
    * ensure that FF type raw does not get mixed with non-raw options
    
    * ensure --delimiter has only one character
    
    * regroup options for datasource push options

--- cmd/datasource_push.go
+++ cmd/datasource_push.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"unicode/utf8"
 
 	"github.com/spf13/cobra"
 	"github.com/stormforger/cli/api"
@@ -20,8 +21,20 @@ var (
 				log.Fatal("Expecting one or more arguments: File(s) to upload")
 			}
 
+			if pushOpts.Raw && (pushOpts.FieldNames != "" || pushOpts.Delimiter != "" || pushOpts.FirstRowHeaders) {
+				log.Fatal("Raw file fixtures do not support --fields, --delimiter and --auto-field-names")
+			}
+
 			if len(args) > 2 && (pushOpts.Name != "" || pushOpts.FieldNames != "") {
 				log.Fatal("--name and --fields is not supported for multiple uploads")
+			}
+
+			if pushOpts.Name != "" && pushOpts.FirstRowHeaders {
+				log.Fatal("--name and --auto-field-names are mutual exclusive")
+			}
+
+			if pushOpts.Delimiter != "" && utf8.RuneCountInString(pushOpts.Delimiter) > 1 {
+				log.Fatal("Delimiter can only be one character!")
 			}
 
 			datasourceOpts.Organisation = lookupOrganisationUID(*NewClient(), args[0])
@@ -32,23 +45,29 @@ var (
 	}
 
 	pushOpts struct {
-		Raw            bool
-		Delimiter      string
-		FieldNames     string
-		Name           string
-		NamePrefixPath string
+		Raw             bool
+		Delimiter       string
+		FieldNames      string
+		Name            string
+		NamePrefixPath  string
+		FirstRowHeaders bool
 	}
 )
 
 func init() {
 	datasourceCmd.AddCommand(datasourcePushCmd)
 
+	// type of FF
 	datasourcePushCmd.Flags().BoolVarP(&pushOpts.Raw, "raw", "r", false, "Upload file as raw fixture")
 
-	datasourcePushCmd.Flags().StringVarP(&pushOpts.Delimiter, "delimiter", "d", "", "Column Delimiter")
+	// general options
 	datasourcePushCmd.Flags().StringVarP(&pushOpts.Name, "name", "n", "", "Name for the file fixture")
 	datasourcePushCmd.Flags().StringVarP(&pushOpts.NamePrefixPath, "name-prefix-path", "p", "", "Prefix for name for the file fixture")
-	datasourcePushCmd.Flags().StringVarP(&pushOpts.FieldNames, "fields", "f", "", "Name for the fields/columns, comma separated")
+
+	// options for structured FF
+	datasourcePushCmd.Flags().StringVarP(&pushOpts.Delimiter, "delimiter", "d", "", "Column Delimiter")
+	datasourcePushCmd.Flags().StringVarP(&pushOpts.FieldNames, "fields", "f", "", "Name for the fields/columns, comma separated (,)")
+	datasourcePushCmd.Flags().BoolVarP(&pushOpts.FirstRowHeaders, "auto-field-names", "", false, "Interpret first row as headers")
 }
 
 func runDataSourcePush(cmd *cobra.Command, args []string) {
@@ -81,10 +100,11 @@ func runDataSourcePush(cmd *cobra.Command, args []string) {
 		fieldNames := pushOpts.FieldNames
 
 		params = &api.FileFixtureParams{
-			Name:       pushOpts.NamePrefixPath + fixtureNameFor(fileName),
-			Type:       fixtureType,
-			FieldNames: fieldNames,
-			Delimiter:  pushOpts.Delimiter,
+			Name:            pushOpts.NamePrefixPath + fixtureNameFor(fileName),
+			Type:            fixtureType,
+			FieldNames:      fieldNames,
+			Delimiter:       pushOpts.Delimiter,
+			FirstRowHeaders: pushOpts.FirstRowHeaders,
 		}
 
 		data, err := os.OpenFile(fileName, os.O_RDONLY, 0755)
