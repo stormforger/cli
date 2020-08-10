7dc9e3b47e731b5a71cb0a493d0554d3cca2d552
Stephan Zeissler <stephan@stormforger.com>
2020-08-10 14:56:57 +0200

    feat(ping): print service account name and uid

--- cmd/ping.go
+++ cmd/ping.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/stormforger/cli/api"
@@ -24,8 +24,21 @@ var (
 	}
 )
 
+func init() {
+	RootCmd.AddCommand(pingCmd)
+
+	pingCmd.Flags().BoolVarP(&pingOpts.Unauthenticated, "unauthenticated", "", false, "Perform unauthenticated ping")
+}
+
 func runPingCmd(cmd *cobra.Command, args []string) {
 	client := NewClient()
+
+	var printPingError func(bool, []byte, error) = printHumanPingError
+	var printPingSuccess func(bool, *api.User) = printHumanPingSuccess
+	if rootOpts.OutputFormat == "json" {
+		printPingError = printJSONPingError
+		printPingSuccess = printJSONPingSuccess
+	}
 
 	var status bool
 	var response []byte
@@ -33,39 +46,74 @@ func runPingCmd(cmd *cobra.Command, args []string) {
 
 	if pingOpts.Unauthenticated {
 		status, response, err = client.PingUnauthenticated()
-
 		if !status {
-			fmt.Println("Could not perform ping! Please verify that you can connect to https://api.stormforger.com")
-			if err != nil {
-				log.Println(err)
-			}
-			log.Fatal(string(response))
-		} else {
-			fmt.Println("PONG!")
+			printPingError(false, response, err)
+			os.Exit(1)
 		}
-
+		printPingSuccess(false, nil)
 	} else {
 		status, response, err = client.Ping()
-
 		if !status {
-			fmt.Println("Could not perform authenticated ping! Please verify that you can login with these credentials at https://app.stormforger.com!")
-			if err != nil {
-				log.Println(err)
-			}
-			log.Fatal(string(response))
-		} else {
-			var data api.User
-			if err := json.NewDecoder(bytes.NewReader(response)).Decode(&data); err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Printf("PONG! Authenticated as %s\n", data.Mail)
+			printPingError(true, response, err)
+			os.Exit(1)
 		}
+		var data api.User
+		if err := json.NewDecoder(bytes.NewReader(response)).Decode(&data); err != nil {
+			printPingError(true, response, err)
+			os.Exit(1)
+		}
+		printPingSuccess(true, &data)
 	}
 }
 
-func init() {
-	RootCmd.AddCommand(pingCmd)
+func printHumanPingSuccess(authenticated bool, data *api.User) {
+	if authenticated {
+		if data.AuthenticatedAs.Type == api.UserTypeServiceAccount {
+			fmt.Printf("PONG! Authenticated as %s (%s)\n", data.AuthenticatedAs.Label, data.AuthenticatedAs.UID)
+		} else {
+			fmt.Printf("PONG! Authenticated as %s\n", data.Mail)
+		}
+	} else {
+		fmt.Println("PONG!")
+	}
+}
 
-	pingCmd.Flags().BoolVarP(&pingOpts.Unauthenticated, "unauthenticated", "", false, "Perform unauthenticated ping")
+func printHumanPingError(authenticated bool, response []byte, err error) {
+	if authenticated {
+		fmt.Print("Could not perform authenticated ping!")
+	} else {
+		fmt.Print("Could not perform ping!")
+	}
+	fmt.Println(" Please verify that you can login with these credentials at https://app.stormforger.com!")
+
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	}
+}
+
+type pingLogMessage struct {
+	Error       bool            `json:"error"`
+	Message     string          `json:"message,omitempty"`
+	APIResponse json.RawMessage `json:"api_response,omitempty"`
+	User        *api.User       `json:"user,omitempty"`
+}
+
+func printJSONPingSuccess(authenticated bool, user *api.User) {
+	logMessage := pingLogMessage{
+		Error:   false,
+		Message: "PONG!",
+		User:    user,
+	}
+	json.NewEncoder(os.Stderr).Encode(logMessage)
+}
+
+func printJSONPingError(_authenticated bool, jsonResponse []byte, err error) {
+	logMessage := pingLogMessage{
+		Error:       true,
+		APIResponse: jsonResponse,
+	}
+	if err != nil {
+		logMessage.Message = err.Error()
+	}
+	json.NewEncoder(os.Stderr).Encode(logMessage)
 }
