b379d43481eb2eb39208e3536e1f1aa21e343ab6
Stephan <stephan@stormforger.com>
2020-08-11 13:31:24 +0200

    feat(cmd/ping):  print service account UID and name instead of email (#168)
    * feat(ping): print service account name and uid
    
    * feat: refactor into a single pingCommandResult printer and added tests
    
    * Update cmd/ping.go

--- api/types.go
+++ api/types.go
@@ -1,6 +1,16 @@
 package api
 
+const (
+	UserTypeServiceAccount = "service-account"
+	UserTypeUser           = "user"
+)
+
 // User describes a API user
 type User struct {
-	Mail string `json:"user_email"`
+	Mail            string `json:"user_email"`
+	AuthenticatedAs *struct {
+		UID   string `json:"uid"`
+		Label string `json:"label"`
+		Type  string `json:"type"` // "service-account" or "user"
+	} `json:"authenticated_as"`
 }
