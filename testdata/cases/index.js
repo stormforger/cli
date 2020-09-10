c7427c79cafed54e5fb8cf19e82cd34ad1e4e0c4
Sebastian Cohnen <tisba@users.noreply.github.com>
2020-09-10 09:50:57 +0200

    WIP

--- testdata/cases/index.js
+++ testdata/cases/index.js
@@ -2,7 +2,7 @@ import "./modules/options.js"
 import scenario from "./modules/scenario.js"
 
 const config = {
-  env: ENV,
+  env: replacements.ENV,
 }
 
 definition.session("hello", scenario(config))
