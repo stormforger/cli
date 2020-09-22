d051c67550be3ea6c6fd0557f4e1658eaf4e6e07
Sebastian Cohnen <tisba@users.noreply.github.com>
2020-09-22 20:41:53 +0200

    refactors ES bundling

--- testdata/cases/index.mjs
+++ testdata/cases/index.mjs
@@ -1,6 +1,10 @@
 import "./modules/options.js"
 import scenario from "./modules/scenario.js"
 
+// TODO do we want a "convention" here? An object
+// that can be used to replace value into, would be a start…
+var replacements = {}
+
 const config = {
   env: replacements.ENV,
 }
