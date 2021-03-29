0d600d716ef8c2daa6b1615d6c502572e69ca027
Stephan Zeissler <stephan@stormforger.com>
2021-03-29 09:14:18 +0200

    add esbuild example to make sure we see at least the most obvious breakage in esbuild output

---
+++ cmd/testcase_build_test.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTestcaseBuild(t *testing.T) {
+	var expectedOutput = GivenTestdataContents(t, t.Name()+"_output.js")
+
+	var output strings.Builder
+	err := MainTestcaseBuild(&output, filepath.Join("testdata", t.Name()+"_main.mjs"), nil)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedOutput, output.String())
+}
+
+func GivenTestdataContents(t *testing.T, file string) string {
+	p := filepath.Join("testdata", file)
+
+	data, err := os.ReadFile(p)
+	require.NoError(t, err)
+
+	return string(data)
+}
+
+func GivenFolder(t *testing.T, files map[string][]byte) string {
+	tempPath := t.TempDir()
+	for name, content := range files {
+		p := path.Join(tempPath, name)
+		err := os.WriteFile(p, content, 0444)
+		require.NoError(t, err)
+	}
+	return tempPath
+}
