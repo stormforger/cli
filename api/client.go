a091e6d7bb8f46470b838c9541a8251df9106afe
Sebastian Cohnen <tisba@users.noreply.github.com>
2023-03-22 18:32:47 +0100

    Remove Public Key Pinning & Update Go (#382)
    * removes public key pinning
    
    * bumps Go version to 1.20
    
    * result of go mod tidy
    
    * applies go fmt changes
    
    * updates Go GHA workflows
    
    * Revert "result of go mod tidy"
    
    This reverts commit 0a65005661a39b63aa75af5bc5b98aae8598cdba.
    
    * go mod tidy
    
    * GHA: with actions/setup-go@v4 cache defaults to true

--- api/client.go
+++ api/client.go
@@ -3,14 +3,10 @@ package api
 import (
 	"bytes"
 	"compress/gzip"
-	"crypto/sha256"
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"io"
 	"log"
 	"mime/multipart"
-	"net"
 	"net/http"
 	"net/textproto"
 	"net/url"
@@ -21,48 +17,10 @@ import (
 	"github.com/stormforger/cli/buildinfo"
 )
 
-type dialer func(network, addr string) (net.Conn, error)
-
-func newDialer(fingerprint []byte, skipCAVerification bool) dialer {
-	return func(network, addr string) (net.Conn, error) {
-		c, err := tls.Dial(network, addr, &tls.Config{InsecureSkipVerify: skipCAVerification})
-		if err != nil {
-			return c, err
-		}
-
-		if addr != "api.stormforger.com:443" {
-			return c, nil
-		} else {
-			connstate := c.ConnectionState()
-			keyPinValid := false
-			for _, peercert := range connstate.PeerCertificates {
-				der, err := x509.MarshalPKIXPublicKey(peercert.PublicKey)
-				hash := sha256.Sum256(der)
-				// log.Println(peercert.Issuer)
-				// log.Printf("%#v\n\n", hash)
-				if err != nil {
-					log.Fatal(err)
-				}
-				if bytes.Compare(hash[0:], fingerprint) == 0 {
-					// log.Println("Pinned Key found")
-					keyPinValid = true
-				}
-			}
-			if keyPinValid == false {
-				log.Fatal("TLS Public Key could not be verified!")
-			}
-			return c, nil
-		}
-	}
-}
-
 func defaultHTTPClient() *http.Client {
-	fingerprint := []byte{0x5b, 0x15, 0x6c, 0xda, 0x7b, 0xc3, 0xd, 0x8b, 0xe8, 0x88, 0x57, 0x75, 0xbc, 0x30, 0xc1, 0x84, 0x18, 0x75, 0x6f, 0x2d, 0x3b, 0x81, 0x91, 0xff, 0x34, 0x10, 0xda, 0x13, 0x4a, 0x83, 0x23, 0x9d}
-
 	tr := &http.Transport{
 		MaxIdleConns:    10,
 		IdleConnTimeout: 30 * time.Second,
-		DialTLS:         newDialer(fingerprint, false),
 		Proxy:           http.ProxyURL(getHTTPProxy()),
 	}
 
