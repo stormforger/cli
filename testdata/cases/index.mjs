fd7c69adb7172b0b1c0c0de4e56cd6fb902611e6
Stephan Zeissler <stephan@stormforge.io>
2023-07-03 18:19:07 +0200

    wording

--- testdata/cases/index.mjs
+++ testdata/cases/index.mjs
@@ -1,8 +1,8 @@
 import "./modules/options.js"
 import scenario from "./modules/scenario.js"
 
-// NOTE: No 'var' or 'const' for defines!!!
-// Inject defines here with '--define defines.env="prod"'
+// NOTE: `--define` works on global identifiers only! To make `defines` a global identifier, it MUST NOT be defined via `var`/`let`/`const`.
+// To replace fields of `defines`, use '--define defines.env="prod"'
 defines = {};
 var config = {
   env: defines.env || "staging",
